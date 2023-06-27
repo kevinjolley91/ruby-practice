827b6f8f0738d7e4e0577be4121adf87a17d4d96
Kevin Jolley <jolley427@gmail.comm>
2023-06-27 15:21:39 -0400

    put item1 into a sentence

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 item1 = { :item_type => "Graphics Card", :color => "RGB", :price => 1400 }
 item2 = { item_type: "CPU", color: "Nickel", price: 570 }
 item3 = { :item_type => "Case", :color => "Black", :price => 180 }
+
+puts "#{item1[:item_type]} costs #{item1[:price]} and is #{item1[:color]}."
