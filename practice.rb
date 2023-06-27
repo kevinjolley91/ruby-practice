17c271406677f3899f11ba91bb1238f9e460ac91
Kevin Jolley <jolley427@gmail.comm>
2023-06-27 19:36:57 -0400

    cuz why not

---
+++ practice.rb
@@ -0,0 +1,24 @@
+# x = 5
+
+# if x == 10
+#   puts 0
+# else
+#   puts -1
+# end
+
+# x = 11
+
+# if x < 10
+#   puts -1
+# else
+#   puts 1
+# end
+
+# x = 5
+# y = 11
+
+# if x < 10 && y < 10
+#   puts 1
+# else
+#   puts 0
+# end
