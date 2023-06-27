451310deabdbee2666e82c9a1f5d7fa8a41951e1
Kevin Jolley <jolley427@gmail.comm>
2023-06-27 15:18:03 -0400

    fixed capitolization

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,3 @@
 item1 = { :item_type => "Graphics Card", :color => "RGB", :price => 1400 }
-item2 = { item_type: "CPU", color: "nickel", price: 570 }
+item2 = { item_type: "CPU", color: "Nickel", price: 570 }
 item3 = { :item_type => "Case", :color => "Black", :price => 180 }
