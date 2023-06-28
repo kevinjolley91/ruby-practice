c0c2b8eb8662672bafe58b069e9da2f6c299eb50
Kevin Jolley <jolley427@gmail.comm>
2023-06-28 13:55:11 -0400

    even more cleaning

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,3 @@
-# item1 = { :item_type => "Graphics Card", :color => "RGB", :price => 1400 }
-# item2 = { item_type: "CPU", color: "Nickel", price: 570 }
-# item3 = { :item_type => "Case", :color => "Black", :price => 180 }
-
-# puts "#{item1[:item_type]} costs #{item1[:price]} and is #{item1[:color]}."
-# puts "#{item2[:item_type]} costs #{item2[:price]} and is #{item2[:color]}."
-# puts "#{item3[:item_type]} costs #{item3[:price]} and is #{item3[:color]}."
-
 class Item
   def initialize(input_item_type, input_color, input_price)
     @item_type = input_item_type
