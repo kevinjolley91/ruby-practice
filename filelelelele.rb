168ec2649800921d1e7289a6682955610c2d699e
Kevin Jolley <jolley427@gmail.comm>
2023-06-29 16:00:55 -0400

    more practicing

---
+++ filelelelele.rb
@@ -0,0 +1,18 @@
+# fav_foods = []
+# 5.times do
+#   puts "What are your favorite foods?:"
+#   answer = gets.chomp
+#   fav_foods << answer
+# end
+
+# i = 0
+# while i < fav_foods.length
+#   puts "I love #{fav_foods[i]}"
+#   i += 1
+# end
+
+# i = 0
+# while i < fav_foods.length
+#   puts "#{i + 1}. #{fav_foods[i]}"
+#   i += 1
+# end
