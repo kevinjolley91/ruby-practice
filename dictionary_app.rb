e892d663cac865af55d2300df694907974fbe1ca
Kevin Jolley <jolley427@gmail.comm>
2023-06-30 14:12:03 -0400

    made loop

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,11 +1,16 @@
 require "http"
 
-puts "Please enter a word:"
-word = gets.chomp
+while true
+  puts "Please enter a word: (enter 'q' when done)"
+  word = gets.chomp
+  if word == "q"
+    break
+  end
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
-dictionary_data = response.parse(:json)
-definitions = dictionary_data[1]["text"]
+  dictionary_data = response.parse(:json)
+  definitions = dictionary_data[1]["text"]
 
-puts "#{word} = #{definitions}"
+  puts "#{word} = #{definitions}"
+end
