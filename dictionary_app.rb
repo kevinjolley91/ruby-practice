9e537b85cca2e5becc8fbbc04fb4f3d5fe295d18
Kevin Jolley <jolley427@gmail.comm>
2023-06-30 14:43:23 -0400

    including pronounciatin

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -5,12 +5,17 @@ while true
   word = gets.chomp
   if word == "q"
     break
+  else
+    response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+
+    response2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=50&api_key=")
+
+    dictionary_data = response.parse(:json)
+    pronounciation_data = response2.parse(:json)
+    definitions = dictionary_data[1]["text"]
+    pronounciation = pronounciation_data[0]["fileUrl"]
+
+    puts "#{word} = #{definitions}"
+    system("open", pronounciation)
   end
-
-  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
-
-  dictionary_data = response.parse(:json)
-  definitions = dictionary_data[1]["text"]
-
-  puts "#{word} = #{definitions}"
 end
