5d708f60cfcd8ce2da9c44eadd6ee533db33563a
Kevin Jolley <jolley427@gmail.comm>
2023-06-29 20:17:27 -0400

    moar bowling

--- bowling_scoring.rb
+++ bowling_scoring.rb
@@ -5,11 +5,11 @@ i = 0
 while i < scores.length
   puts "How many pins did you knock down on first ball?:"
   roll = gets.chomp.to_i
-  if roll == 10
+  # if roll == 10
   scores[i] = scores[i] + roll
   puts "How many pins did you knock down on second ball?:"
   roll2 = gets.chomp.to_i
-  if roll + roll2 == 10
+  # if roll + roll2 == 10
   scores2[i] = scores2[i] + roll2
   i += 1
 end
