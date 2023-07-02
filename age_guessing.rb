f4464aadd5471d41b9e8d6ac17e29cbafb0b08b7
Kevin Jolley <jolley427@gmail.comm>
2023-07-02 19:42:10 -0400

    added gender

--- age_guessing.rb
+++ age_guessing.rb
@@ -1,9 +1,12 @@
 require "http"
 
-puts "What is your name? I'll try to guess your age."
+puts "What is your name? I'll try to guess your age and gender."
 name = gets.chomp
 
 response = HTTP.get("https://api.agify.io?name=#{name}")
 guess = response.parse(:json)["age"]
 
-puts guess
+response = HTTP.get("https://api.genderize.io?name=#{name}")
+guess2 = response.parse(:json)["gender"]
+
+puts "You are #{guess} years old and are #{guess2}!"
